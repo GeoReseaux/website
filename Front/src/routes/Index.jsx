33c02e6535b0fcffa9f396b22a52473049c29249
nffdev <nightfrost2discord@outlook.fr>
2025-11-05 13:16:33 +0100

    feat(home): updated text

--- Front/src/routes/Index.jsx
+++ Front/src/routes/Index.jsx
@@ -56,7 +56,7 @@ export default function Home() {
                     <ArrowRight className="ml-2 h-4 w-4 transition-transform group-hover:translate-x-1" />
                   </Button>
                   <Button variant="outline" size="lg">
-                    Nos services
+                    Nos prestations
                   </Button>
                 </motion.div>
               </motion.div>
